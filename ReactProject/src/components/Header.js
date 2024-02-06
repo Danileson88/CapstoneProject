3b4085ae4aa0391dccc902d2b691c1cb8b6b7847
Danilel <DanielOrtega8788@outlook.com>
2024-02-05 22:56:01 -0600

    added more styling

--- ReactProject/src/components/Header.js
+++ ReactProject/src/components/Header.js
@@ -4,7 +4,16 @@ import React from "react";
 export default function Header() {
     return(
         <header className="Header">
-        
+            <div className="header-text">
+                <h1 className="header-title">Little Lemon</h1>
+                <h2 className="header-city">Chicago</h2>    
+                <h3 className="header-description">We are a Family owned Mediterranean
+                restaurant, focused on traditional recipies
+                served with a modern twist.
+                </h3>
+                <button className="Reserve-btn">Reserve a Table</button>
+            </div>
+            <img src="images/Hero-image.png" className="Hero-img"/>
         </header>
     )
 };
