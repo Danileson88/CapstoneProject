895f4238176df4b6f7cf5e7e91df338ae55aae3e
Danilel <DanielOrtega8788@outlook.com>
2024-02-10 19:33:26 -0600

    2/10 project update

--- ReactProject/src/components/Main.js
+++ ReactProject/src/components/Main.js
@@ -7,17 +7,35 @@ export default function Main() {
                 <h1 className="main-specials-txt">This Weeks Specials!</h1>
                 <button className="main-menu-btn" >online Menu</button>
             </div>
-            <div className="main-card">
+            <div className="grid-card">
                 <div className="card-1">
                     <img className="greeksalad-img" src="images/Greek-salad.png"/>
-                    <h2>Greek Salad <span className="cost">12.99</span></h2>
+                    <div className="card-details">
+                        <h2>Greek Salad</h2>
+                        <p>$12.99</p>
+                        <p className="card-text">The famous greek salad of crispy lettuce,
+                            peppers, Olives and our Chicago style feta
+                            cheese,garnishd with crucny garlic and rosemary
+                            croutons.
+                        </p>
+                        <h3>Order Delivery</h3>
+                        <img className="delivery-icon" src="images/delivery-icon.png" alt="delivery-icon"/>
+                    </div>    
                 </div>
                 <div className="card-2">
                     <img className="bruschetta-img" src="images/Bruschetta.png" alt="Bruschetta dish"/>
+                    <div className="card-details">
+                        <h2>Bruschetta</h2>
+                        <p>$12.99</p>
+                    </div>
                 </div>
                 <div className="card-3">
-                    <img className="dessert-img" src="images/Lemon-dessert.png" alt="a slice of lemon dessert"/>
-                </div>                
+                    <img className="dessert-img" src="images/Lemon-dessert.png" alt="a slice of lemon dessert"/>                
+                    <div className="card-details">
+                        <h2>Lemon Cake</h2>
+                        <p>$12.99</p>
+                    </div>
+                </div>
             </div>                
         </main>
     )
