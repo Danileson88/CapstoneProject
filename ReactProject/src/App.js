253ca79d7ce80a230e9fa313067d9147ac3880ab
Danilel <DanielOrtega8788@outlook.com>
2024-02-14 22:01:32 -0600

    started working on testimonials

--- ReactProject/src/App.js
+++ ReactProject/src/App.js
@@ -3,6 +3,7 @@ import Footer from'./components/Footer'
 import Header from'./components/Header'
 import Main from'./components/Main'
 import Nav from'./components/Nav'
+import Testimonials from'./components/Testimonials'
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Nav/>
       <Header/>
       <Main/>
+      <Testimonials/>
       <Footer/>
     </div>
 )}
---
+++ ReactProject/src/components/Testimonials.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Testimonials() {
+
+    return(
+        <div className="testimonails-container">
+            <div>
+                <h3 className="rating-txt">Rating<span className="rating">5</span></h3>
+                <img src="images/profile1.jpg" width={100}/>
+            </div>
+        </div>
+    )
+};
